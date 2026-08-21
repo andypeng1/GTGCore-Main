1ceb41942f98d0528016d9d762860e3c0c27b3dc
andypeng1NB <2458702358@qq.com>
2026-08-21 13:21:22 +0800

    Fixed All Failure
    # Conflicts:
    #	build.gradle

--- src/main/java/com/andypeng1/gtgcore/GtgCore.java
+++ src/main/java/com/andypeng1/gtgcore/GtgCore.java
@@ -1,40 +1,41 @@
 package com.andypeng1.gtgcore;
 
+import com.gregtechceu.gtceu.api.machine.MachineDefinition;
+import com.gregtechceu.gtceu.api.registry.GTRegistries;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.RegisterEvent;
 import org.slf4j.Logger;
 
-/**
- * GTG（格雷科技：银河 / GregTech: Galaxy）整合包核心模组。
- * modid: gtgcore
- */
 @Mod(GtgCore.MODID)
-public class GtgCore
-{
+@Mod.EventBusSubscriber(modid = GtgCore.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class GtgCore {
     public static final String MODID = "gtgcore";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public GtgCore(FMLJavaModLoadingContext context)
-    {
+    public GtgCore(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
-        modEventBus.addListener(this::commonSetup);
-
-        MachineRegistrate.init();
-
-        // 注册 ForgeConfigSpec，Forge 会负责加载/保存配置文件
+        // 注册配置
         context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
 
+        // 注意：不再需要在构造函数中添加监听器，因为 @SubscribeEvent 会处理
+        // 但仍需将 Mod 实例注册到 Forge 事件总线（如果有需要的话）
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-        LOGGER.info("GTG Core (GregTech: Galaxy) loaded");
+    // 改为监听注册表事件，在 GTCEu 的 Machine 注册表解冻之前执行
+    @SubscribeEvent
+    public static void onRegisterMachine(RegisterEvent event) {
+        // 判断是否为 MachineDefinition 注册表
+        if (event.getRegistryKey().equals(GTRegistries.MACHINES)) {
+            LOGGER.info("GTG Core: Registering custom machines...");
+            MachineRegistrate.init();
+        }
     }
 }
--- src/main/java/com/andypeng1/gtgcore/MachineRegistrate.java
+++ src/main/java/com/andypeng1/gtgcore/MachineRegistrate.java
@@ -1,45 +1,48 @@
 package com.andypeng1.gtgcore;
+
 import com.andypeng1.gtgcore.machines.LargeGasCollector;
 import com.gregtechceu.gtceu.api.data.RotationState;
 import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
 import com.gregtechceu.gtceu.api.machine.multiblock.PartAbility;
-import com.gregtechceu.gtceu.api.machine.multiblock.WorkableElectricMultiblockMachine;
 import com.gregtechceu.gtceu.api.pattern.FactoryBlockPattern;
 import com.gregtechceu.gtceu.api.pattern.Predicates;
-import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
 import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
-import com.gregtechceu.gtceu.api.machine.multiblock.MultiblockControllerMachine;
-import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
-import com.gregtechceu.gtceu.common.registry.GTRegistration;
 import com.gregtechceu.gtceu.common.data.GTBlocks;
 import com.gregtechceu.gtceu.common.data.GTRecipeTypes;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 
 import static com.gregtechceu.gtceu.api.pattern.Predicates.blocks;
 import static com.gregtechceu.gtceu.api.pattern.Predicates.controller;
 
 public class MachineRegistrate {
-    public static void init() {}
+    // 只保留 GTRegistrate 实例，不进行任何注册调用
     public static final GTRegistrate REGISTRATE = GTRegistrate.create("gtgcore");
-    public static final MultiblockMachineDefinition LARGE_GAS_COLLECTOR = REGISTRATE
-            .multiblock("large_gas_collector", LargeGasCollector::new)
-            .langValue("Large Gas Collector")
-            .rotationState(RotationState.ALL)
-            .recipeType(GTRecipeTypes.GAS_COLLECTOR_RECIPES)
-            .pattern(definition -> FactoryBlockPattern.start()
-                    .aisle("XXXXX", "XXXXX", "XXXXX", "XXXXX", "XXXXX")
-                    .aisle("XVVVX", "AGGGV", "AGGGV", "VGGGV", "XVVVX")
-                    .aisle("XVVVX", "AGGGV", "AGSGV", "VGGGV", "XVVVX")
-                    .aisle("XVVVX", "AGGGV", "AGGGV", "VGGGV", "XVVVX")
-                    .aisle("XXXXX", "XXXXX", "AGCGV", "AGGGA", "XXXXX")
-                    .where('C', controller(blocks(definition.get())))
-                    .where("X", Predicates.blocks(GTBlocks.CASING_STEEL_SOLID.get())
-                            .or(Predicates.autoAbilities(definition.getRecipeTypes()))
-                            .or(Predicates.autoAbilities(true, true, true)))
-                    .where("V", blocks(GTBlocks.CASING_ASSEMBLY_LINE.get()))
-                    .where("G", blocks(GTBlocks.CASING_TUNGSTENSTEEL_PIPE.get()))
-                    .where("S", blocks(GTBlocks.FUSION_CASING_MK3.get()))
-                    .build())
-            .register();
+
+    // 机器定义声明为静态，但先赋值为 null，稍后在 init() 中创建
+    public static MultiblockMachineDefinition LARGE_GAS_COLLECTOR = null;
+
+    // 此方法在 Mod 生命周期事件中调用，此时 GTCEu 已完全初始化
+    public static void init() {
+        LARGE_GAS_COLLECTOR = REGISTRATE
+                .multiblock("large_gas_collector", LargeGasCollector::new)
+                .langValue("Large Gas Collector")
+                .rotationState(RotationState.ALL)
+                .recipeType(GTRecipeTypes.GAS_COLLECTOR_RECIPES)
+                .pattern(definition -> FactoryBlockPattern.start()
+                        .aisle("XXXXX", "XXXXX", "XXXXX", "XXXXX", "XXXXX")
+                        .aisle("XVVVX", "AGGGV", "AGGGV", "VGGGV", "XVVVX")
+                        .aisle("XVVVX", "AGGGV", "AGSGV", "VGGGV", "XVVVX")
+                        .aisle("XVVVX", "AGGGV", "AGGGV", "VGGGV", "XVVVX")
+                        .aisle("XXXXX", "XXXXX", "AGCGV", "AGGGA", "XXXXX")
+                        .where('C', controller(blocks(definition.get())))
+                        .where("X", Predicates.blocks(GTBlocks.CASING_STEEL_SOLID.get())
+                                .or(Predicates.abilities(PartAbility.MAINTENANCE).setMinGlobalLimited(1))
+                                .or(Predicates.abilities(PartAbility.INPUT_ENERGY).setMaxGlobalLimited(2))
+                                .or(Predicates.autoAbilities(definition.getRecipeTypes()))
+                                .or(Predicates.autoAbilities(true, true, true)))
+                        .where("V", blocks(GTBlocks.CASING_ASSEMBLY_LINE.get()))
+                        .where("G", blocks(GTBlocks.CASING_TUNGSTENSTEEL_PIPE.get()))
+                        .where("S", blocks(GTBlocks.FUSION_CASING_MK3.get()))
+                        .build())
+                .register();
+    }
 }
