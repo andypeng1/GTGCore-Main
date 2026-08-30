9a65975c4a86b302b9d34ae0b36011d4f305a9a1
andypeng1NB <2458702358@qq.com>
2026-08-30 22:04:59 +0800

    修复大型集气室：侧壁纹理、结构朝向、并行仓支持与终端乱放仓室问题，新增中文翻译和机器描述

--- src/main/java/com/andypeng1/gtgcore/MachineRegistrate.java
+++ src/main/java/com/andypeng1/gtgcore/MachineRegistrate.java
@@ -6,12 +6,15 @@ import com.gregtechceu.gtceu.api.data.RotationState;
 import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
 import com.gregtechceu.gtceu.api.machine.multiblock.PartAbility;
 import com.gregtechceu.gtceu.api.pattern.FactoryBlockPattern;
+import com.gregtechceu.gtceu.api.pattern.util.RelativeDirection;
 import com.gregtechceu.gtceu.api.pattern.Predicates;
 import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
 import com.gregtechceu.gtceu.common.data.GTBlocks;
 import com.gregtechceu.gtceu.common.data.GTMachines;
 import com.gregtechceu.gtceu.common.data.GTRecipeModifiers;
 import com.gregtechceu.gtceu.common.data.GTRecipeTypes;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 
 import static com.gregtechceu.gtceu.api.pattern.Predicates.blocks;
 import static com.gregtechceu.gtceu.api.pattern.Predicates.controller;
@@ -26,12 +29,15 @@ public class MachineRegistrate {
         LARGE_GAS_COLLECTOR = REGISTRATE
                 .multiblock("large_gas_collector", LargeGasCollector::new)
                 .langValue("Large Gas Collector")
-                .rotationState(RotationState.ALL)
+                .tooltips(
+                        Component.literal("超频不会损失能效").withStyle(ChatFormatting.GOLD),
+                        Component.literal("通过并行控制仓让机器同时处理多个相同配方"),
+                        Component.literal("可用配方类型：集气室"))
                 .recipeType(GTRecipeTypes.GAS_COLLECTOR_RECIPES)
-                .recipeModifiers(GTRecipeModifiers.OC_PERFECT)
+                .recipeModifiers(GTRecipeModifiers.OC_PERFECT, GTRecipeModifiers.PARALLEL_HATCH)
                 .appearanceBlock(GTBlocks.CASING_STEEL_SOLID)
-                .rotationState(RotationState.Y_AXIS)
-                .pattern(definition -> FactoryBlockPattern.start()
+                .rotationState(RotationState.NON_Y_AXIS)
+                .pattern(definition -> FactoryBlockPattern.start(RelativeDirection.LEFT, RelativeDirection.FRONT, RelativeDirection.UP)
                         .aisle("XXXXX", "XXXXX", "XXXXX", "XXXXX", "XXXXX")
                         .aisle("XVVVX", "VGGGV", "VGGGV", "VGGGV", "XVVVX")
                         .aisle("XVVVX", "VGGGV", "VGSGV", "VGGGV", "XVVVX")
@@ -45,10 +51,7 @@ public class MachineRegistrate {
                                         .setMinGlobalLimited(1)
                                         .setMaxGlobalLimited(2))
                                 .or(Predicates.abilities(PartAbility.EXPORT_FLUIDS).setExactLimit(1))
-                                .or(Predicates.abilities(PartAbility.IMPORT_FLUIDS).setMaxGlobalLimited(0))
                                 .or(Predicates.abilities(PartAbility.IMPORT_ITEMS).setMaxGlobalLimited(1))
-                                .or(Predicates.abilities(PartAbility.EXPORT_ITEMS).setMaxGlobalLimited(0))
-                                .or(Predicates.abilities(PartAbility.MUFFLER).setMaxGlobalLimited(0))
                                 .or(Predicates.abilities(PartAbility.PARALLEL_HATCH).setMaxGlobalLimited(1)))
                         .where("V", blocks(GTBlocks.CASING_GRATE.get()))
                         .where("G", blocks(GTBlocks.CASING_TUNGSTENSTEEL_PIPE.get()))
