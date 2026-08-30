9ed400f4dbbf711b69610e50de907ff5237c5bee
andypeng1NB <2458702358@qq.com>
2026-08-30 22:11:42 +0800

    删除远程引入的devtools开发工具文件

--- devtools/DeleteFiles.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-public class DeleteFiles {
-    public static void main(String[] args) {
-        for (String p : args) {
-            File f = new File(p);
-            System.out.println((f.delete() ? "DELETED " : "FAILED  ") + p);
-        }
-    }
-}
